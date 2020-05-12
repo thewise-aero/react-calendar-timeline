827b3b84c3b74c1779239e028a31bf6c987147c2
danielthewise <65236621+danielthewise@users.noreply.github.com>
2020-05-12 12:20:36 -0400

    Fix Ctrl + Scroll Zoom Jump

--- src/lib/Timeline.js
+++ src/lib/Timeline.js
@@ -538,7 +538,7 @@ export default class ReactCalendarTimeline extends Component {
   }
 
   handleWheelZoom = (speed, xPosition, deltaY) => {
-    this.changeZoom(1.0 + speed * deltaY / 500, xPosition / this.state.width)
+    this.changeZoom((speed === 10 ? 1.5 : 1.0) + speed * deltaY / 500, xPosition / this.state.width)
   }
 
   changeZoom = (scale, offset = 0.5) => {
